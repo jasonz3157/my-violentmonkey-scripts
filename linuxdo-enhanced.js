6cbfcd18534eb2d120e45ba32618f1aff1f2d2c2
jasonz3157 <48433202+jasonz3157@users.noreply.github.com>
2026-08-10 16:04:16 +0800

    蓝色标记楼主

--- linuxdo-enhanced.js
+++ linuxdo-enhanced.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         LINUX DO Enhanced
 // @namespace    my-violentmonkey-scripts
-// @version      0.1.0
+// @version      0.1.1
 // @description  增强 LINUX DO 的话题浏览体验，将楼主用户名标记为红色。
 // @author       jasonz3157
 // @match        https://linux.do/t/*
-// @icon         https://linux.do/favicon.ico
+// @icon         data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz48c3ZnIHZlcnNpb249IjEuMiIgYmFzZVByb2ZpbGU9InRpbnktcHMiIHdpZHRoPSIxMjgiIGhlaWdodD0iMTI4IiB2aWV3Qm94PSIwIDAgMTIwIDEyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48dGl0bGU+TElOVVggRE8gTG9nbzwvdGl0bGU+PGNsaXBQYXRoIGlkPSJhIj48Y2lyY2xlIGN4PSI2MCIgY3k9IjYwIiByPSI0NyIvPjwvY2xpcFBhdGg+PGNpcmNsZSBmaWxsPSIjZjBmMGYwIiBjeD0iNjAiIGN5PSI2MCIgcj0iNTAiLz48cmVjdCBmaWxsPSIjMWMxYzFlIiBjbGlwLXBhdGg9InVybCgjYSkiIHg9IjEwIiB5PSIxMCIgd2lkdGg9IjEwMCIgaGVpZ2h0PSIzMCIvPjxyZWN0IGZpbGw9IiNmMGYwZjAiIGNsaXAtcGF0aD0idXJsKCNhKSIgeD0iMTAiIHk9IjQwIiB3aWR0aD0iMTAwIiBoZWlnaHQ9IjQwIi8+PHJlY3QgZmlsbD0iI2ZmYjAwMyIgY2xpcC1wYXRoPSJ1cmwoI2EpIiB4PSIxMCIgeT0iODAiIHdpZHRoPSIxMDAiIGhlaWdodD0iMzAiLz48L3N2Zz4=
 // @grant        GM_addStyle
 // @run-at       document-start
 // @license      GPL-3.0
@@ -16,7 +16,7 @@
 (function () {
   'use strict';
 
-  const TOPIC_OWNER_USERNAME_COLOR = '#e53935';
+  const TOPIC_OWNER_USERNAME_COLOR = '#00aeff';
 
   GM_addStyle(`
     .topic-owner .topic-meta-data .names a[data-user-card] {
